9a7ad464e599c74c94a9b979ac034639ddf797a9
James <nukajames@gmail.com>
2020-08-20 11:24:35 -0500

    Ejercicio R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Esta función toma una matriz cuadrada y la convierte en una 
+## matriz especial que guarda en caché como inversa
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function()
+    x
+  setinverse <- function(inverse)
+    m <<- inverse
+  getinverse <- function()
+    m
+  list(
+    set = set,
+    get = get,
+    setinverse = setinverse,
+    getinverse = getinverse
+  )
+  
 }
 
 
-## Write a short comment describing this function
+## Esta función devuelve el cálculo de la inversa y lo carga o 
+## lo trae del caché
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if (!is.null(m)) {
+    message("Matriz recuperada del caché")
+    return(solve(m)) ## Retorna la matriz original
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  message("Matriz inversa")
+  m  ## Retorna la matriz inversa de 'x'
 }
